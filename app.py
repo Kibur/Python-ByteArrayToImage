58b76940a868e80e47ff448342769f0f7c048203
Kibur <manu2293@hotmail.com>
2015-01-01 19:13:43 +0100

    ByteArrayToImage Converter

---
+++ app.py
@@ -0,0 +1,74 @@
+__author__ = 'Kibur'
+
+import os
+import io
+import sys
+import Image
+from gi.repository import Gtk
+import hashlib
+import base64
+
+class UI:
+    # Handlers
+    def window_close(self, *args):
+	try:
+		os.remove(self.filename)
+
+		print 'Removed "' + self.filename + '"'
+	except OSError, ose: # No such file or directory
+		pass
+
+        Gtk.main_quit(args)
+
+    def on_button_clicked(self, button):
+        textBuffer = self.txtInput.get_buffer()
+	start, end = textBuffer.get_bounds()
+	data = textBuffer.get_text(start, end, False)
+
+	if data.startswith('0x'):
+		data = data[2:]
+
+	try:
+		byteArray = bytearray.fromhex(data)
+		bytesIO = io.BytesIO(byteArray)
+        	image = Image.open(bytesIO)
+		msgd5 = hashlib.md5(str(image)).digest()
+		encoded = base64.b16encode(msgd5)
+
+		self.filename = encoded + '.png'
+
+		image.save(self.filename)
+
+		print 'Temporary file "' + self.filename + '" created!'
+
+		self.imgOutput.set_from_file(self.filename)
+	except ValueError, ve:
+		print 'Corrupt data or not an image'
+	except IOError, ioe:
+		print 'Corrupt data or not an image'
+    # ---
+
+    def connectHandlers(self):
+        self.window.connect('delete-event', self.window_close)
+        self.btnConvert.connect('clicked', self.on_button_clicked)
+
+    def retreiveObjects(self):
+        self.window = self.builder.get_object("window1")
+        self.txtInput = self.builder.get_object('txtInput')
+        self.btnConvert = self.builder.get_object('btnConvert')
+        self.imgOutput = self.builder.get_object('imgOutput')
+
+    def __init__(self):
+        self.builder = Gtk.Builder()
+        self.builder.add_from_file("view.glade")
+
+        self.retreiveObjects()
+        self.connectHandlers()
+
+        self.window.show()
+
+	self.filename = 'void'
+
+if __name__ == "__main__":
+    ui = UI()
+    Gtk.main()
